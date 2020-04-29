47ad9485e4757d80e4cb616afbe3d265d15af553
Fred Bitenyo <fredbitenyo@gmail.com>
2020-04-29 22:17:24 +0200

    set up home page, menu functionalities, add names columns

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'devise'
 gem 'twitter-bootstrap-rails'
 gem 'devise-bootstrap-views'
 gem 'jquery-rails'
+gem 'hirb'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
+
+  def full_name
+    return "#{self.first_name.capitalize} #{self.last_name.capitalize}" if first_name || last_name
+    "Anonymous"
+  end
 end
